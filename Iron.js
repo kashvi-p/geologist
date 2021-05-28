4e30c6915d33ec1257cdf9b585f559cdd0d471d8
kashvi-p <80035295+kashvi-p@users.noreply.github.com>
2021-05-28 11:34:21 +0530

    Add files via upload

---
+++ Iron.js
@@ -0,0 +1,26 @@
+class Iron{
+	constructor(x,y,width,height)
+	{
+	var options={
+		restitution:0.8,
+		friction:3,
+		density:30
+	}
+		this.body=Bodies.rectangle(x, y, width, height, options)
+		this.width = width;
+        this.height = height;
+		World.add(world, this.body);
+	}
+	display()
+	{
+			var ironpos=this.body.position;		
+			push()
+			translate(ironpos.x, ironpos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			fill("black");
+			rect(0, 0, this.width, this.height);
+			pop();
+	}
+}
---
+++ Stone.js
@@ -0,0 +1,27 @@
+class Stone{
+	constructor(x,y,width,height)
+	{
+	var options={
+		restitution:0.8,
+		friction:0.9,
+		density:12
+	}
+		this.body=Bodies.rectangle(x, y, width, height, options)
+        this.width = width;
+        this.height = height;
+		World.add(world, this.body);
+	}
+	display()
+	{
+			var stonepos=this.body.position;		
+			push()
+			translate(stonepos.x, stonepos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			fill("brown");
+			rect(0, 0, this.width, this.height);
+			pop()
+	}
+
+}
